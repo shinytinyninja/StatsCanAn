4ddf944a52f8635c7e2bf0f3f46a61b2747a05b8
Jeffrey Wong <jeffreywong155@gmail.com>
2020-04-15 16:24:38 -0400

    Structure

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-import pandas as pandas
+import pandas as pd
+import matplotlib as mp
+class main:
+
+    def __init__():
+
+    def sortByRain(self, value):
+
+    def sortBySun(self, value):
+
+    def correlation(self):
+        # builtin panda function
+        #co2 levels and rain days
+
+    def getMean():
+        return(csv.getMean)
+
+    def rainBarGraphTrend():
+
+    def sunBarGraphTrend():
+
+    def temperature():
+
+    def summerTemperatures():
+
+    def predictNextYear():
+
+
+def main():
+    fileName = input("Enter the name of the CSV")
+    main(fileName)
+    sort by rain
+    sort by sun
+
+
+if __name__ == '__main__':
+    main()
+    
